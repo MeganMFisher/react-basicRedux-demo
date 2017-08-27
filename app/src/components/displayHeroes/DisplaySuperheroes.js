4bb098fb0ade39d37e0b279f69e299be73dbcceb
MeganMFisher <meganmichelle318@gmail.com>
2017-08-27 09:22:26 -0600

    removed hero!

--- app/src/components/displayHeroes/DisplaySuperheroes.js
+++ app/src/components/displayHeroes/DisplaySuperheroes.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { removeHero } from '../../ducks/superheroes';
 
 class DisplaySuperheroes extends Component {
+
+
     render() {
-        console.log(this.props.heroes)
     
         const heroes = this.props.heroes.map((hero , i) => {
             return (
             <div key={i}>
                 <h4>{ hero.name }</h4>
                 <h6>{ hero.power }</h6>
+                <button onClick={()=> this.props.removeHero(i)}>Remove</button>
             </div>
             )
         })
-    
+        
         return (
             <div>
                 <h1>All the Superheroes</h1>
@@ -31,5 +34,5 @@ function mapStateToProps(state) {
 
 
 
-export default connect(mapStateToProps)(DisplaySuperheroes)
+export default connect(mapStateToProps, {removeHero})(DisplaySuperheroes)
 
--- app/src/ducks/superheroes.js
+++ app/src/ducks/superheroes.js
@@ -1,12 +1,14 @@
 const ADD_HERO = 'ADD_HERO';
+const REMOVE_HERO = 'REMOVE_HERO';
 
 export default function reducer(state, action) {
     switch(action.type) {
         case ADD_HERO:
-        const hero = Object.assign({}, action.payload)
-        return {
-            heroes: [...state.heroes, hero]
-        }
+            return Object.assign({}, state, {heroes: [...state.heroes, action.payload]});
+            
+        case REMOVE_HERO:
+            return Object.assign({}, state, {heroes: state.heroes.filter((hero, i) => i !== action.payload)});
+
         default:
             return state;
     }
@@ -17,5 +19,12 @@ export function addHero(hero) {
     return {
         type: ADD_HERO,
         payload: hero
+    }
+}
+
+export function removeHero(i) {
+    return {
+        type: REMOVE_HERO,
+        payload: i
     }
 }
